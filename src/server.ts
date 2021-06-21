a6a77a0e09502f10ceba38dab833fed641840eb8
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-21 19:00:52 -0300

    Install and configure cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.listen(process.env.PORT, () => console.log('Server is running!'));
