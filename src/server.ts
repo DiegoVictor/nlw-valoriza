443ababc8ed4839965dfecd5c85043a0e5664299
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-21 19:01:39 -0300

    Install and configure helmet

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
 
 app.listen(process.env.PORT, () => console.log('Server is running!'));
