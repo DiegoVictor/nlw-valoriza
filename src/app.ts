902752c2ce905e6735cd3ba63165dd11fb9a1a35
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-22 20:32:24 -0300

    Create error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import 'reflect-metadata';
+import 'express-async-errors';
 
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import { errors } from 'celebrate'
+import { isBoom } from '@hapi/boom';
 
 import './database';
 
@@ -19,5 +21,25 @@ app.use(express.json());
 app.use('/v1', router);
 
 app.use(errors());
+app.use(
+  async (
+    error: Error,
+    _: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    if (isBoom(error)) {
+      const { statusCode, payload } = error.output;
+
+      return response.status(statusCode).json({
+        ...payload,
+        ...error.data,
+        docs: process.env.DOCS_URL,
+      });
+    }
+
+    return next(error);
+  }
+);
 
 export { app }
