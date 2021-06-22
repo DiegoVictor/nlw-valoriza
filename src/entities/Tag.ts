a7a932d25849ae259b395ce28f76fc2f4c46dff0
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-22 20:40:05 -0300

    Create tag entity

---
+++ src/entities/Tag.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn
+} from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+@Entity('tags')
+export class Tag {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
