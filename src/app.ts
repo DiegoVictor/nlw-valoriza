4c45817e1c43f779f75dd85a3660ad2a2e051b41
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-21 22:21:28 -0300

    Load routes

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,15 @@ import cors from 'cors';
 import helmet from 'helmet';
 
 import './database';
+
+import { router } from './routes';
+
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+
+app.use('/v1', router);
 
 export { app }
