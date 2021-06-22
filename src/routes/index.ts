5067769599d5bb22f1e7918c30b32ed269066505
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-22 17:59:45 -0300

    Create user validation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+
 import { CreateUserController } from '../controllers/CreateUserController';
+import userValidator from '../validators/userValidator';
 
 const router = Router();
 
 const createUserController = new CreateUserController()
 
-router.post('/users', createUserController.handle);
+router.post('/users', userValidator, createUserController.handle);
 
 export { router };
---
+++ src/validators/userValidator.ts
@@ -0,0 +1,10 @@
+import { celebrate, Joi, Segments } from "celebrate";
+
+export default celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6),
+    admin: Joi.boolean().optional()
+  })
+})
