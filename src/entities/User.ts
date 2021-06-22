400edaa7fcc8f04b1bea8cfde4e4ddeb55eb1604
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-21 21:28:07 -0300

    Create user migration and entity

---
+++ src/entities/User.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn
+} from "typeorm";
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { User };
