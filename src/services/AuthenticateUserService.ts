d90b9a35c581d4e0a516cd94f822ed0acbacb9f9
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-23 20:32:46 -0300

    Create authentication service

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,39 @@
+import { badRequest } from '@hapi/boom';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { Repository } from 'typeorm';
+import { User } from '../entities/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+class AuthenticateUserService {
+  private usersRepository: Repository<User>;
+
+  constructor(usersRepository: Repository<User>) {
+    this.usersRepository = usersRepository;
+  }
+
+  async execute({ email, password }: Request): Promise<string> {
+    const user = await this.usersRepository.findOne({ email });
+
+    if (!user) {
+      throw badRequest('Email or password is incorret');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw badRequest('Email or password is incorret');
+    }
+
+    const token = sign({ email }, process.env.JWT_SECRET, {
+      subject: user.id,
+      expiresIn: process.env.JWT_EXPIRATION_TIME,
+    });
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService };
