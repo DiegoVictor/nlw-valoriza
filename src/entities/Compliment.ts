17687f57d946e82e7c6148a5c40bc327beb7acd1
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-23 20:50:01 -0300

    Create compliments model

---
+++ src/entities/Compliment.ts
@@ -0,0 +1,45 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Tag } from './Tag';
+import { User } from './User';
+
+@Entity('compliments')
+export class Compliment {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  user_sender: string;
+
+  @JoinColumn({ name: 'user_sender' })
+  @ManyToOne(() => User)
+  userSender: User;
+
+  @Column()
+  user_receiver: string;
+
+  @JoinColumn({ name: 'user_receiver' })
+  @ManyToOne(() => User)
+  userReceiver: User;
+
+  @Column()
+  tag_id: string;
+
+  @JoinColumn({ name: 'tag_id' })
+  @ManyToOne(() => Tag)
+  tag: Tag;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    this.id = uuid();
+  }
+}
