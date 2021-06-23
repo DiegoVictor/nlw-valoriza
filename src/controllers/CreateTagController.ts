234cdf78e5d85ee67ae5a2e58faa4a3212f636f8
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-22 21:01:43 -0300

    Create controller to create tag

---
+++ src/controllers/CreateTagController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+
+import { TagsRepositories } from "../repositories/TagsRepositories";
+import { CreateTagService } from "../services/CreateTagService";
+
+class CreateTagController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name } = request.body;
+
+    const tagsRepositories = getCustomRepository(TagsRepositories);
+    const createTagService = new CreateTagService(tagsRepositories);
+
+    const tag = await createTagService.execute(name);
+
+    return response.json(tag)
+  }
+}
+
+export { CreateTagController }
