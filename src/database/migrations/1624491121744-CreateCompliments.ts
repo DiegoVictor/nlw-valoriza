8205836863371857e75c50e037f2601230a0aef1
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-23 20:44:40 -0300

    Create compliments migration

---
+++ src/database/migrations/1624491121744-CreateCompliments.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCompliments1624491121744 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'compliments',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'user_sender',
+            type: 'uuid',
+          },
+          {
+            name: 'user_receiver',
+            type: 'uuid',
+          },
+          {
+            name: 'tag_id',
+            type: 'uuid',
+          },
+          {
+            name: 'message',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKUserSenderCompliments',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_sender'],
+            onUpdate: 'SET NULL',
+            onDelete: 'SET NULL',
+          },
+          {
+            name: 'FKUserReceiverCompliments',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_receiver'],
+            onUpdate: 'SET NULL',
+            onDelete: 'SET NULL',
+          },
+          {
+            name: 'FKTagCompliments',
+            referencedTableName: 'tags',
+            referencedColumnNames: ['id'],
+            columnNames: ['tag_id'],
+            onUpdate: 'SET NULL',
+            onDelete: 'SET NULL',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('compliments');
+  }
+}
