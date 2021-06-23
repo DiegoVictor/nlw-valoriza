191174c54ad52777251e101d52109981094f0427
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-23 20:33:39 -0300

    Create route to login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,11 +5,20 @@ import { CreateTagController } from '../controllers/CreateTagController';
 import userValidator from '../validators/userValidator';
 import nameValidator from '../validators/nameValidator';
 import ensureAdmin from '../middlewares/ensureAdmin';
+import { AuthenticateUserController } from '../controllers/AuthenticateUserController';
+import emailAndPasswordValidator from '../validators/emailAndPasswordValidator';
 
 const router = Router();
 
+const authenticateUserController = new AuthenticateUserController();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+
+router.post(
+  '/sessions',
+  emailAndPasswordValidator,
+  authenticateUserController.handle
+);
 
 router.post('/users', userValidator, createUserController.handle);
 
