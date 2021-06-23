488a899d545aa40cded757b3159fbbf25b48bc1c
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-22 21:01:18 -0300

    Create tag service

---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,25 @@
+import { badRequest } from "@hapi/boom";
+import { Repository } from "typeorm";
+
+import { Tag } from "../entities/Tag";
+
+class CreateTagService {
+  private tagsRepository: Repository<Tag>;
+
+  constructor(tagsRepository: Repository<Tag>) {
+    this.tagsRepository = tagsRepository;
+  }
+
+  async execute (name: string):Promise<Tag> {
+    if (await this.tagsRepository.findOne({ name })) {
+      throw badRequest('Tag already exists', { code: 240 })
+    };
+
+    const tag = this.tagsRepository.create({ name });
+    await this.tagsRepository.save(tag);
+
+    return tag;
+  }
+}
+
+export { CreateTagService }
