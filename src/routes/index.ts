f65a9a69ed1f6eeb7f8fba8c66e69bda34c91767
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-22 21:01:33 -0300

    Create route to create tag

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '../controllers/CreateUserController';
+import { CreateTagController } from '../controllers/CreateTagController';
 import userValidator from '../validators/userValidator';
+import nameValidator from '../validators/nameValidator';
+import ensureAdmin from '../middlewares/ensureAdmin';
 
 const router = Router();
 
 const createUserController = new CreateUserController()
+const createTagController = new CreateTagController()
 
 router.post('/users', userValidator, createUserController.handle);
+
+router.post('/tags', ensureAdmin, nameValidator, createTagController.handle);
 
 export { router };
