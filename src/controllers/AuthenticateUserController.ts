054d376dd8162819b70665210d806524d403b976
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-23 20:33:01 -0300

    Create authentication controller

---
+++ src/controllers/AuthenticateUserController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepositories } from '../repositories/UsersRepositories';
+import { AuthenticateUserService } from '../services/AuthenticateUserService';
+
+class AuthenticateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const usersRepositories = getCustomRepository(UsersRepositories);
+    const authenticateUserService = new AuthenticateUserService(
+      usersRepositories
+    );
+
+    const accessToken = await authenticateUserService.execute({
+      email,
+      password,
+    });
+
+    return response.json({ access_token: accessToken });
+  }
+}
+
+export { AuthenticateUserController };
