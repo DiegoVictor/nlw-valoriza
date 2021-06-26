ac050ddae806e975715bd7023771f0b435a8ddd2
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-26 00:22:19 -0300

    Create util to generate links header data

---
+++ src/utils/PaginationLinks.ts
@@ -0,0 +1,31 @@
+interface PageLinks {
+  first?: string;
+  prev?: string;
+  next?: string;
+  last?: string;
+}
+
+class PaginationLinks {
+  static generate(
+    page: number,
+    pagesTotal: number,
+    resourceUrl: string
+  ): PageLinks {
+    const url = `${resourceUrl}?page=`;
+    const links: PageLinks = {};
+
+    if (page < pagesTotal) {
+      links.last = url + pagesTotal;
+      links.next = url + (page + 1);
+    }
+
+    if (page > 1) {
+      links.first = `${url}1`;
+      links.prev = url + (page - 1);
+    }
+
+    return links;
+  }
+}
+
+export { PaginationLinks };
