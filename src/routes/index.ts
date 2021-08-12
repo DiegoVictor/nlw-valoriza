74499f8952e842e359ba44f1194a21398ef918f4
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-08-12 19:30:48 -0300

    Rename variable

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,11 +5,11 @@ import { tagsRoutes } from './tags';
 import { complimentsRoutes } from './compliments';
 import { sessionsRoutes } from './sessions';
 
-const router = Router();
+const routes = Router();
 
-router.use('/sessions', sessionsRoutes);
-router.use('/users', usersRoutes);
-router.use('/tags', tagsRoutes);
-router.use('/compliments', complimentsRoutes);
+routes.use('/sessions', sessionsRoutes);
+routes.use('/users', usersRoutes);
+routes.use('/tags', tagsRoutes);
+routes.use('/compliments', complimentsRoutes);
 
-export { router };
+export { routes };
